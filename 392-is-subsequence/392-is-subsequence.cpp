291478b8ec3f739047a892889207b4c220316f37
AYUSHBHASKARSINGH <95188806+AYUSHBHASKARSINGH@users.noreply.github.com>
2022-07-20 20:54:32 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (28.52%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -6,12 +6,9 @@ public:
         
         while(i<t.size() && j<s.size()){
             if(t[i] == s[j]){
-                i++;
                 j++;
             }
-            else{
                 i++;
-            }
         }
         if(j==s.size()){
             return true;

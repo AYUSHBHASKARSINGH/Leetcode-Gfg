a366dd92039783d7f04aa6e720479d26ad0e7f8d
AYUSHBHASKARSINGH <95188806+AYUSHBHASKARSINGH@users.noreply.github.com>
2023-07-02 00:34:24 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -5,21 +5,52 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 class Solution{
+private:
+    int helper(vector<vector<int> > arr, int n, int m){
+        int low = 0;
+        int high = m-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[n][mid]==0){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return m-low;
+    }
+    
 public:
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
 	    // code here
 	    int maxi = 0;
 	    int ind = -1;
-	    for(int i=0;i<n;i++){
-	        auto temp = upper_bound(arr[i].begin(),arr[i].end(),0) - arr[i].begin();
-	        int yy = m-temp;
+	   // for(int i=0;i<n;i++){
+	   //     auto temp = upper_bound(arr[i].begin(),arr[i].end(),0) - arr[i].begin();
+	   //     int yy = m-temp;
 	        
-	        if(yy>maxi){
-	            maxi = yy;
-	            ind = i;
-	        }
-	    }
-	    return ind;
+	   //     if(yy>maxi){
+	   //         maxi = yy;
+	   //         ind = i;
+	   //     }
+	   // }
+	   // return ind;
+	   
+	   
+	   //WITHOUT UPPER BOUND
+	   
+	   for(int i=0;i<n;i++){
+	       int temp = helper(arr,i,m);
+	       //cout<<"ans-->"<<temp<<endl;
+	       if(temp>maxi){
+	           maxi = temp;
+	           ind = i;
+	       }
+	   }
+	   return ind;
+	   
 	}
 
 };

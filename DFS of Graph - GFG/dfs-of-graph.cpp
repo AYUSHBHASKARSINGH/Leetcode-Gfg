6a3c107968fe4931710276f6fd1d382d1bf3ddbf
AYUSHBHASKARSINGH <95188806+AYUSHBHASKARSINGH@users.noreply.github.com>
2023-07-10 18:32:33 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.cpp
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -4,29 +4,54 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+    
+    
   public:
+    void dfs(vector<int> &ans,vector<int> adj[],vector<bool> &visited,int node){
+            ans.push_back(node);
+            visited[node] = true;
+             
+            // HAR CONNECTED NODE K LIYE RECURSIVE CALL
+            for(int i=0;i<adj[node].size();i++){
+                if(!visited[adj[node][i]]){
+                    dfs(ans,adj,visited,adj[node][i]);
+                }
+            }
+    }
+    
     // Function to return a list containing the DFS traversal of the graph.
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        vector<bool> vis(V,false);
-        stack<int> st;
+        // vector<bool> vis(V,false);
+        // stack<int> st;
+        // vector<int> ans;
+        // st.push(0);
+        // while(!st.empty()){
+        //     int top = st.top();
+        //     st.pop();
+            
+        //     if(!vis[top]){
+        //         ans.push_back(top);
+        //         vis[top] = true;
+        //     }
+            
+        //     for(int i=adj[top].size()-1;i>=0;i--){
+        //         if(!vis[adj[top][i]]){
+        //             st.push(adj[top][i]);
+        //         }
+        //     }
+        // }
+        
+        // return ans;
+        
+        
+        
+        // -----USING RECURRSION
+        
         vector<int> ans;
-        st.push(0);
-        while(!st.empty()){
-            int top = st.top();
-            st.pop();
-            
-            if(!vis[top]){
-                ans.push_back(top);
-                vis[top] = true;
-            }
-            
-            for(int i=adj[top].size()-1;i>=0;i--){
-                if(!vis[adj[top][i]]){
-                    st.push(adj[top][i]);
-                }
-            }
-        }
+        vector<bool> visited(V,false);
+        
+        dfs(ans,adj,visited,0);
         
         return ans;
     }
